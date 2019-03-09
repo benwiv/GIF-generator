999b2e116d77cfb4ba6ba61875088fa34bf467bc
Ben Waller <benwalleriv@gmail.com>
2019-03-08 22:25:30 -0800

    first major updates

---
+++ scripts/generator.js
@@ -0,0 +1,26 @@
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=EZGhOkGlbqQxMlmd8TcKduknfKl1CYVn";
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+});
+
+//  list of my interests
+let myList = ['music', 'dancing', 'film', 'science fiction', 'france', 'coffee'];
+
+
+//  animate or still function
+$(".gif").on("click", function() {
+  var state = $(this).attr("data-state");
+
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } 
+  else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+});
