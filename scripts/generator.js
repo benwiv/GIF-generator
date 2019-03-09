609129d178f5e009ef782bf21669c9edce02333d
Ben Waller <benwalleriv@gmail.com>
2019-03-09 11:05:57 -0800

    JS updated, almost complete

--- scripts/generator.js
+++ scripts/generator.js
@@ -1,14 +1,32 @@
-var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=EZGhOkGlbqQxMlmd8TcKduknfKl1CYVn";
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
+$("#add-movie").on("click", function() {
+  var topic = $(this).attr("data-topic");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=twinpeaks" +
+    topic + "&api_key=dc6zaTOxFJmzC&limit=10";
+  let myList = ['music', 'space', 'film', 'science', 'france', 'coffee'];
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+  
+  .then(function(response) {
+  var results = response.data;
+
+  for (var i = 0; i < results.length; i++) {
+    if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+      var gifDiv = $("<div class='resultGIF'>");
+      var rating = results[i].rating;
+      var p = $("<p>").text("Rating: " + rating);
+      var personImage = $("<img>");
+      personImage.attr("src", results[i].images.fixed_height.url);
+      gifDiv.append(p);
+      gifDiv.append(personImage);
+      $("#gif-display-div").prepend(gifDiv);
+    }
+  }
+  });
 });
-
-//  list of my interests
-let myList = ['music', 'space', 'film', 'science', 'france', 'coffee'];
 
 
 //  animate or still function
